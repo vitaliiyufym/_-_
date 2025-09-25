6ba86c6cb31abc4ab0e9e8e26e0cc8249cea419a
Task_1 <Vitalii_Yufym.KB.2025@lpnu.ua>
2025-09-25 17:30:43 +0300

    Hi

---
+++ main.py
@@ -0,0 +1,8 @@
+first_name = input("Ввеіть своє ім'я: ")
+last_name = input("Введіть своє прізвище: ")
+birthday = input("Введіть свій рік народження: ")
+print(f"Прізвище: {last_name}")
+print(f"Ім’я: {first_name}")
+print(f"Рік народження: {birthday}")
+print(f"{last_name} {first_name}, {birthday} р. н.")
+
