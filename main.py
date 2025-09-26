21833af288519fe8d335676333ee16b29c9a8c5b
vitaliiyufym <vitalii.yufym.kb.2025@lpnu.ua>
2025-09-26 11:14:23 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,8 +1,12 @@
-first_name = input("Ввеіть своє ім'я: ")
-last_name = input("Введіть своє прізвище: ")
-birthday = input("Введіть свій рік народження: ")
-print(f"Прізвище: {last_name}")
-print(f"Ім’я: {first_name}")
-print(f"Рік народження: {birthday}")
-print(f"{last_name} {first_name}, {birthday} р. н.")
-
+formula1 = "s = s0 + v0*t + (1/2)*g*t**2"
+print("Шлях:")
+print(formula1)
+formula2 = "FV = PV * (1 + INT/100)**YRS"
+print("\n2.Вартость (FV):")
+print(formula2)
+formula3 = "G = 4 * math.pi**2 * a**3 / (p**2 * (m1 + m2))"
+print("\n3.Гравітація (G):")
+print(formula3)
+formula4 = "c = math.sqrt(a**2 + b**2 - 2*a*b * math.cos(gamma))"
+print("\n4.Косинус:")
+print(formula4)
---
+++ pepe.py
@@ -0,0 +1,8 @@
+a = int(input("a = "))
+b = int(input("b = "))
+
+x = a - b
+y = (x**2)**0.5
+maxim = (a + b + y) // 2
+
+print("Найбільше число =", int(maxim))
